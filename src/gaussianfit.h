5c281b3800dc127a73d9342287f5cba6d5a7b9af
Sebastien Fabbro <sfabbro@uvic.ca>
2013-02-12 13:51:08 -0800

    first round of the poloka-sub module

--- src/gaussianfit.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAUSSIANFIT__H
-#define GAUSSIANFIT__H
-
-
-double gaussianfit(const double *values, int nval, double &mean, double &sigma, const double &k, bool first_evalutation);
-
-
-#endif /* GAUSSIANFIT__H */
